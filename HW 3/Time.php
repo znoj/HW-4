af4ab14fc5457e1c3d48ede3b3f204370d4f8f91
Manzhos Anton <amanzhos@ukr.net>
2015-05-19 19:17:59 +0300

    added time.php

---
+++ HW 3/Time.php
@@ -0,0 +1,14 @@
+<?php
+$hour = date("H");
+if (($hour >= 5) && ($hour <= 10)) {
+	echo "Good Morning";
+}
+elseif (($hour >= 11) && ($hour <= 16)) {
+	echo "Good afternoon";
+}
+elseif (($hour >= 17) && ($hour <= 21)) {
+	echo "Good evening";
+}
+elseif (($hour >= 22) || ($hour < 5)) {
+	echo "GoodNight"; 
+} 
